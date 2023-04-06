947fb5c82737071844f0a1e78621d5eed55e3c61
heteyszabolcs <heteysz@gmail.com>
2023-04-06 15:48:03 +0200

    heatmap update for unsorted dataset

--- utils/bw_heatmap_overlap_NPC_epigmarkers.sh
+++ utils/bw_heatmap_overlap_NPC_epigmarkers.sh
@@ -12,17 +12,17 @@ module load R_packages
 
 cd /proj/snic2020-6-3/SZABOLCS/LTRIS2_BRG1_H33_and_G4s/utils
 
-computeMatrix reference-point -o ../results/deeptools/matrix_mESC_NPC_epigmarkers.mat.gz -S \
- ../data/bw/GSM4303808_WT-H3K4me3-1_mm10.bigwig \
-  ../data/bw/GSM4303796_WT-H3K4me1-1_mm10.bigwig \
- ../data/bw/GSM4377779_WT-H3K27me3-1_mm10.bigwig \
- ../data/bw/GSM4205678_WT-H3K27ac-1.10M_depth_mm10.bigwig \
- -R ../results/GenomicRanges/unsorted_outputs/bulk_overlaps/MACS2_cl0_only_K4me3_signal_sorted.bed \
- ../results/GenomicRanges/unsorted_outputs/bulk_overlaps/MACS2_intersection_K4me3_signal_sorted.bed \
- ../results/GenomicRanges/unsorted_outputs/bulk_overlaps/MACS2_cl1_only_K4me3_signal_sorted.bed \
- --referencePoint center \
- --sortRegions keep \
- -b 3000 -a 3000 --samplesLabel "H3K4me3" "H3K4me1" "H3K27me3" "H3K27ac" --skipZeros --missingDataAsZero
+# computeMatrix reference-point -o ../results/deeptools/matrix_mESC_NPC_epigmarkers.mat.gz -S \
+ # ../data/bw/GSM4303808_WT-H3K4me3-1_mm10.bigwig \
+  # ../data/bw/GSM4303796_WT-H3K4me1-1_mm10.bigwig \
+ # ../data/bw/GSM4377779_WT-H3K27me3-1_mm10.bigwig \
+ # ../data/bw/GSM4205678_WT-H3K27ac-1.10M_depth_mm10.bigwig \
+ # -R ../results/GenomicRanges/unsorted_outputs/bulk_overlaps/MACS2_cl0_only_K4me3_signal_sorted.bed \
+ # ../results/GenomicRanges/unsorted_outputs/bulk_overlaps/MACS2_intersection_K4me3_signal_sorted.bed \
+ # ../results/GenomicRanges/unsorted_outputs/bulk_overlaps/MACS2_cl1_only_K4me3_signal_sorted.bed \
+ # --referencePoint center \
+ # --sortRegions keep \
+ # -b 3000 -a 3000 --samplesLabel "H3K4me3" "H3K4me1" "H3K27me3" "H3K27ac" --skipZeros --missingDataAsZero
 
 plotHeatmap -m ../results/deeptools/matrix_mESC_NPC_epigmarkers.mat.gz \
  -out "../results/deeptools/mESC_NPC_ol_epigmarkers.pdf" \
@@ -31,8 +31,8 @@ plotHeatmap -m ../results/deeptools/matrix_mESC_NPC_epigmarkers.mat.gz \
  --heatmapHeight 14 \
  --colorMap "Blues" \
  --yMin 0 \
- --yMax 15 \
- --zMax 15 \
+ --yMax 30 \
+ --zMax 30 \
  -z "cluster 0" "both" "cluster 1" \
  --yAxisLabel "" \
  --xAxisLabel "" \
@@ -45,8 +45,8 @@ plotHeatmap -m ../results/deeptools/matrix_mESC_NPC_epigmarkers.mat.gz \
  --heatmapHeight 14 \
  --colorMap "Blues" \
  --yMin 0 \
- --yMax 15 \
- --zMax 15 \
+ --yMax 30 \
+ --zMax 30 \
  -z "cluster 0" "both" "cluster 1" \
  --yAxisLabel "" \
  --xAxisLabel "" \
--- utils/bw_heatmap_unsorted.sh
+++ utils/bw_heatmap_unsorted.sh
@@ -12,36 +12,36 @@ module load R_packages
 
 cd /proj/snic2020-6-3/SZABOLCS/LTRIS2_BRG1_H33_and_G4s/utils
 
-computeMatrix reference-point -o ../results/deeptools/matrix_unsorted_PQS.mat.gz -S \
- ../data/bw/mm10_canPQS-regex_binary.bw \
+computeMatrix reference-point -o ../results/deeptools/matrix_unsorted.mat.gz -S \
+ ../results/Seurat/callpeaks_unsorted/cluster_bigwigs/unsorted.bw \
  -R ../results/Seurat/callpeaks_unsorted/peak_sets/0_peaks_res0.1.bed \
  ../results/Seurat/callpeaks_unsorted/peak_sets/1_peaks_res0.1.bed \
  --referencePoint center \
- -b 3000 -a 3000 --samplesLabel "ext. PQS" --skipZeros --missingDataAsZero
+ -b 3000 -a 3000 --samplesLabel "G4" --skipZeros --missingDataAsZero
 
-plotHeatmap -m ../results/deeptools/matrix_unsorted_PQS.mat.gz \
- -out "../results/deeptools/unsorted_clusters_PQS.pdf" \
+plotHeatmap -m ../results/deeptools/matrix_unsorted.mat.gz \
+ -out "../results/deeptools/unsorted_clusters.pdf" \
  --refPointLabel "G4" \
  --heatmapHeight 14 \
  --yMin 0 \
- --yMax 0.01 \
+ --yMax 15 \
  -z "cluster 0" "cluster 1" \
  --whatToShow "heatmap only" \
- --colorMap "binary" \
+ --colorMap "Blues" \
  --yAxisLabel "" \
  --xAxisLabel "" \
  --legendLocation "none"
 
-plotHeatmap -m ../results/deeptools/matrix_unsorted_PQS.mat.gz \
- -out "../results/deeptools/unsorted_clusters_PQS.png" \
+plotHeatmap -m ../results/deeptools/matrix_unsorted.mat.gz \
+ -out "../results/deeptools/unsorted_clusters.png" \
  --refPointLabel "G4" \
  --heatmapHeight 14 \
  --yMin 0 \
- --yMax 0.01 \
+ --yMax 15 \
  --sortUsing "max" \
  -z "cluster 0" "cluster 1" \
  --whatToShow "heatmap only" \
- --colorMap "binary" \
+ --colorMap "Blues" \
  --yAxisLabel "" \
  --xAxisLabel "" \
  --legendLocation "none"
